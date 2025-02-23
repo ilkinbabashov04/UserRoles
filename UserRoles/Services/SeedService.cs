e6c9b11bbe22348047d4a43e54873eb0391e8f3f
ilkinbabashov04 <ilkinbabasov99@gmail.com>
2025-02-23 12:28:39 +0400

    Initial commit

---
+++ UserRoles/Services/SeedService.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Identity;
+using UserRoles.Data;
+using UserRoles.Models;
+
+namespace UserRoles.Services
+{
+    public class SeedService
+    {
+        public static async Task SeedDatabase(IServiceProvider serviceProvider)
+        {
+            using var scope = serviceProvider.CreateScope();
+            var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+            var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+            var userManager = scope.ServiceProvider.GetRequiredService<UserManager<Users>>();
+            var logger = scope.ServiceProvider.GetRequiredService<ILogger<SeedService>>();
+
+            try
+            {
+                logger.LogInformation("Ensuring the databasse is created ");
+                await context.Database.EnsureCreatedAsync();
+
+                logger.LogInformation("Sending Roles");
+                await AddRoleAsync(roleManager, "Admin");
+                await AddRoleAsync(roleManager, "User");
+
+                logger.LogInformation("Seeding admin user.");
+                var adminEmail = "ilkinbabasov99@gmail.com";
+                if(await userManager.FindByEmailAsync(adminEmail) == null)
+                {
+                    var adminUser = new Users
+                    {
+                        FullName = "Ilkin Babashov",
+                        UserName = adminEmail,
+                        NormalizedUserName = adminEmail.ToUpper(),
+                        Email = adminEmail,
+                        NormalizedEmail = adminEmail.ToUpper(),
+                        EmailConfirmed = true,
+                        SecurityStamp = Guid.NewGuid().ToString(),
+                    };
+
+                    var result = await userManager.CreateAsync(adminUser, "Admin@123");
+                    if(result.Succeeded)
+                    {
+                        logger.LogInformation("Assigning Admin role to the admin user");
+                        await userManager.AddToRoleAsync(adminUser, "Admin");
+                    }
+                    else
+                    {
+                        logger.LogError("Failed to create admin user: {Errors}", string.Join(", ", result.Errors.Select(e => e.Description)));
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, "An error occured while seeding the database.");
+            }
+        }
+
+        private static async Task AddRoleAsync(RoleManager<IdentityRole> roleManager, string roleName)
+        {
+            if(!await roleManager.RoleExistsAsync(roleName))
+            {
+                var result = await roleManager.CreateAsync(new IdentityRole(roleName));
+                if(!result.Succeeded)
+                {
+                    throw new Exception($"Failed to create role '{roleName}': {string.Join(",", result.Errors.Select(e => e.Description))}");
+                }
+            }
+        }
+    }
+}
---
+++ UserRoles/ViewModels/LoginViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UserRoles.ViewModels
+{
+    public class LoginViewModel
+    {
+
+        [Required(ErrorMessage = "Email is requiresd.")]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required.")]
+        [DataType(DataType.Password)]
+        public string Paassword { get; set; }
+
+        [Display(Name = "Remember me?")]
+        public bool RememberMe { get; set; }
+    }
+}
